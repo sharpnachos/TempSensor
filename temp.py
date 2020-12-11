4a77b451a11542bc9caadaf55d83e41be4c72666
Thomas Walsh <lwalsh7@luc.edu>
2020-12-11 02:58:05 -0600

    Twitter bot support

--- temp.py
+++ temp.py
@@ -1,5 +1,11 @@
 import subprocess
 import time
+import tweepy
+
+auth = tweepy.OAuthHandler("w4J98Qk4qkD7yDAlVHb98p36b", "MnGJdovG5pdXUkETkxokGZTottP7G99p1mzLAYJULr7VI8JFkE")
+auth.set_access_token("1302514377283731456-NqHIy9sBpxbdqIN9acZGzKbJpjTbT3", "HyVguC43mBsmpkaHuakb2yc9eI8KW1rNQ3PO40JewTyju")
+
+api = tweepy.API(auth)
 
 def main():
     bashCommand = "sudo i2cget -y 1 0x48"
@@ -8,6 +14,7 @@ def main():
     tempC = hexConvert(output)
     tempF = tempConvert(tempC)
     uploadToDB(tempF)
+    tweet(tempF)
     print("Temp Recorded!")
     time.sleep(21600)
     main()
@@ -22,5 +29,8 @@ def tempConvert(tempC):
 
 def uploadToDB(tempF):
     pass
+
+def tweet(tempF):
+    api.update_status("The current temperature is " + str(tempF) + " degrees farenheit.")
 
 main()
